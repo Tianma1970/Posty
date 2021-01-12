72286a4ab086cce014baf52dbd8890394cf7068b
Tillmann Weimer <tillmann1970@gmail.com>
2021-01-12 12:48:08 +0100

    moved links into conditional statements

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -22,18 +22,26 @@
         </ul>
 
         <ul class="flex items-center">
-            <li>
-                <a href class="p-3"="">Tillmann Weimer</a>
-            </li>
-            <li>
-                <a href class="p-3"="">Login</a>
-            </li>
-            <li>
-                <a href class="p-3"="">Logout</a>
-            </li>
-            <li>
-                <a href="{{ route('register') }}" class="p-3">Register</a>
-            </li>
+            @auth
+                <li>
+                    <a href class="p-3"="">Tillmann Weimer</a>
+                </li>
+                <li>
+                    <a href class="p-3"="">Logout</a>
+                </li>
+            @endauth
+            @guest
+                <li>
+                    <a href ="{{ route('login') }}" class="p-3">Login</a>
+                </li>
+
+                <li>
+                    <a href="{{ route('register') }}" class="p-3">Register</a>
+                </li>
+
+            @endguest
+
+
         </ul>
     </nav>
     @yield('content')
