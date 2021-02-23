d6a1688e735723d184c3715eb8728d877f6fb065
Tillmann Weimer <tillmann1970@gmail.com>
2021-02-23 10:30:20 +0100

    added delete post only for own posts

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -27,6 +27,15 @@
                     <div class="mb-4">
                         <a href="" class="font-bold">{{ $post->user->name }}</a><span class="text-gray-600 text-sm">&nbsp;{{ $post->created_at->diffForHumans() }}</span>
                         <p class="mb-2">{{ $post->body }}</p>
+                        @if($post->ownedBy(auth()->user()))
+                            <div>
+                                <form action="{{ route('posts.destroy', $post) }}" method="post">
+                                    @csrf
+                                    @method('DELETE')
+                                    <button type="submit" class="text-blue-500">Delete</button>
+                                </form>
+                            </div>
+                        @endif
 
                         <div class="flex items-center">
                             @auth
@@ -42,7 +51,7 @@
                                     <button type="submit" class="text-blue-500">Unlike</button>
                                     </form>
                                 @endif
-                            @endauth
+                                @endauth
                             <span>{{ $post->likes->count() }} {{ Str::plural('like', $post->likes->count() )}}</span>
                         </div>
                     </div>
