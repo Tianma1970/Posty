897232ef809ec278555c6020e8510d992ed10222
Tillmann Weimer <tillmann1970@gmail.com>
2021-02-26 14:08:13 +0100

    added variable to be sent to view

--- app/Http/Controllers/UserPostController.php
+++ app/Http/Controllers/UserPostController.php
@@ -10,11 +10,11 @@ class UserPostController extends Controller
 {
     public function index(User $user, Post $post)
     {
-        // $posts = $user->posts()->with(['user', 'likes'])->paginate(20);
+        $posts = $user->posts()->with(['user', 'likes'])->paginate(20);
 
         return view('users.posts.index', [
             'user'  => $user,
-            // 'posts' => $posts
+            'posts' => $posts
         ]);
     }
 }
